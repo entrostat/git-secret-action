f82815502c59949f16c55eb9e724d22992615f8a
Kerren <kerren@entrostat.com>
2022-03-16 17:39:00 +0200

    Try escape the pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,11 @@ echo "Revealing the secrets in the repository..."
 echo "$1" | gpg --no-tty --batch --import
 
 if [ ! -z "$2" ]; then
-  pass_arg="-p \"${2@Q}\""
+  # Check this link to see why we use the @Q
+  # https://unix.stackexchange.com/questions/379181/escape-a-variable-for-use-as-content-of-another-script
+  q_mid=\'\\\'\'
+  pass_esc="'${2//\'/$q_mid}'"
+  pass_arg="-p \"${pass_esc}\""
 fi
 
 git secret reveal $pass_arg
