8a32eaec99dc9185fb46b8a496287404734058c1
Kerren <kerren@entrostat.com>
2022-03-16 17:33:14 +0200

    Escape the passphrase in the bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Revealing the secrets in the repository..."
 echo "$1" | gpg --no-tty --batch --import
 
 if [ ! -z "$2" ]; then
-  pass_arg="-p $2"
+  pass_arg="-p \"${2@Q}\""
 fi
 
 git secret reveal $pass_arg
