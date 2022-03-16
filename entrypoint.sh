08e7956cc5988da4d41cea409cb4d2bb808c3c80
Kerren <kerren@entrostat.com>
2022-03-16 17:47:54 +0200

    Rather wrap the passphrase in single quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,13 @@ echo "Revealing the secrets in the repository..."
 
 echo "$1" | gpg --no-tty --batch --import
 
-if [ ! -z "$2" ]; then
+q_mid=\'\\\'\'
+pass_esc="'${2//\'/$q_mid}'"
+if [ ! -z "$pass_esc" ]; then
   # Check this link to see why we use the @Q
   # https://unix.stackexchange.com/questions/379181/escape-a-variable-for-use-as-content-of-another-script
-  q_mid=\'\\\'\'
-  pass_esc="'${2//\'/$q_mid}'"
-  pass_arg="-p \"${pass_esc}\""
+
+  pass_arg="-p ${pass_esc}"
 fi
 
 git secret reveal $pass_arg
