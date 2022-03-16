e36a70d2a2af45d733149d80d0a77674b9118852
Kerren <kerren@entrostat.com>
2022-03-16 22:33:56 +0200

    Try standard double quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,11 @@ echo "Revealing the secrets in the repository..."
 
 echo "$1" | gpg --no-tty --batch --import
 
-q_mid=\'\\\'\'
-pass_esc="'${2//\'/$q_mid}'"
-if [ "$pass_esc" != "''" ]; then
+if [ ! -z "$2" ]; then
   # Check this link to see why we use the @Q
   # https://unix.stackexchange.com/questions/379181/escape-a-variable-for-use-as-content-of-another-script
 
-  pass_arg="-p ${pass_esc}"
+  pass_arg="-p \"$2\""
 fi
 
 git secret reveal $pass_arg
