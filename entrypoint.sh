2d8d20c5775e934acb37752fe4242b0d1a2bc0da
Kerren <kerren@entrostat.com>
2022-03-18 14:36:23 +0200

    Try use %s instead of %q

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ if [ ! -z "$2" ]; then
   # Check this link to see why we use the @Q
   # https://unix.stackexchange.com/questions/379181/escape-a-variable-for-use-as-content-of-another-script
 
-  printf %q "$2" | xargs git secret reveal -p
+  printf "%s" "$2" | xargs git secret reveal -p
 else
   git secret reveal
 fi
