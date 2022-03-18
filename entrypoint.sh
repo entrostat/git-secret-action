3a42c2b81449810ef01b042eac967440bd56002c
Kerren <kerren@entrostat.com>
2022-03-18 14:41:53 +0200

    fix(passphrase): Removed comments that were not longer applicable

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,6 @@ echo "Revealing the secrets in the repository..."
 echo "$1" | gpg --no-tty --batch --import
 
 if [ ! -z "$2" ]; then
-  # Check this link to see why we use the @Q
-  # https://unix.stackexchange.com/questions/379181/escape-a-variable-for-use-as-content-of-another-script
-
   escaped_pass=$(printf "%s" "$2")
   git secret reveal -p "${escaped_pass}"
 else
