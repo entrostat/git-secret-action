1a592ea2201383f6098d63736a3f2b2f09eac488
Kerren <kerren@entrostat.com>
2022-03-18 14:39:07 +0200

    Wrap the passphrase in quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ if [ ! -z "$2" ]; then
   # Check this link to see why we use the @Q
   # https://unix.stackexchange.com/questions/379181/escape-a-variable-for-use-as-content-of-another-script
 
-  git secret reveal -p $(printf "%s" "$2")
+  escaped_pass=$(printf "%s" "$2")
+  git secret reveal -p "${escaped_pass}"
 else
   git secret reveal
 fi
