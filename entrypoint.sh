922eddd29c5c12d4216c20a662f024e800dc8569
Kerren <kerren@entrostat.com>
2022-04-19 13:40:00 +0200

    chore: Cleaned up the logs and outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 cd /github/workspace
 git config --global --add safe.directory /github/workspace
-echo "The repository files are as follows:"
-ls -lah
 
 echo "Revealing the secrets in the repository..."
 
@@ -11,11 +9,7 @@ echo "$1" | gpg --no-tty --batch --import
 
 if [ ! -z "$2" ]; then
   escaped_pass=$(printf "%s" "$2")
-  cd /github/workspace
-  git status
   git secret reveal -p "${escaped_pass}"
 else
-  cd /github/workspace
-  git status
   git secret reveal
 fi
